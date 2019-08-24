d43c0c4ab5c43c90dd0f168fc349541efc072b65
jperez1486 <jerryperez06@gmail.com>
2019-08-24 17:25:20 -0400

    Add files via upload

---
+++ automate-iis-install.ps1
@@ -0,0 +1,10 @@
+## Automate the install of IIS on a VM
+
+Set-AzVMExtension -ResourceGroupName "myResourceGroupAutomate" `
+    -ExtensionName "IIS" `
+    -VMName "myVM" `
+    -Location "EastUS" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'

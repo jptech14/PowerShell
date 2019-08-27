1024b9432127b65ffce164f1d2da67694b231850
jperez1486 <jerryperez06@gmail.com>
2019-08-26 21:26:45 -0400

    Add files via upload

---
+++ deploy-web-application-to-a-scale-set.ps1
@@ -0,0 +1,156 @@
+# Create a new scale set
+
+New-AzVmss `
+  -ResourceGroupName "myResourceGroupScaleSet" `
+  -Location "EastUS" `
+  -VMScaleSetName "myScaleSet" `
+  -VirtualNetworkName "myVnet" `
+  -SubnetName "mySubnet" `
+  -PublicIpAddressName "myPublicIPAddress" `
+  -LoadBalancerName "myLoadBalancer" `
+  -UpgradePolicyMode "Automatic"
+
+# Deploy a sample application 
+
+# Define the script for your Custom Script Extension to run
+$publicSettings = @{
+    "fileUris" = (,"https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate-iis.ps1");
+    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File automate-iis.ps1"
+}
+
+# Get information about the scale set
+$vmss = Get-AzVmss `
+  -ResourceGroupName "myResourceGroupScaleSet" `
+  -VMScaleSetName "myScaleSet"
+
+# Use Custom Script Extension to install IIS and configure basic website
+Add-AzVmssExtension -VirtualMachineScaleSet $vmss `
+  -Name "customScript" `
+  -Publisher "Microsoft.Compute" `
+  -Type "CustomScriptExtension" `
+  -TypeHandlerVersion 1.8 `
+  -Setting $publicSettings
+
+# Update the scale set and apply the Custom Script Extension to the VM instances
+Update-AzVmss `
+  -ResourceGroupName "myResourceGroupScaleSet" `
+  -Name "myScaleSet" `
+  -VirtualMachineScaleSet $vmss
+
+# Allow traffic to the application
+
+# Get information about the scale set
+$vmss = Get-AzVmss `
+  -ResourceGroupName "myResourceGroupScaleSet" `
+  -VMScaleSetName "myScaleSet"
+
+#Create a rule to allow traffic over port 80
+$nsgFrontendRule = New-AzNetworkSecurityRuleConfig `
+  -Name myFrontendNSGRule `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 200 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access Allow
+
+#Create a network security group and associate it with the rule
+$nsgFrontend = New-AzNetworkSecurityGroup `
+  -ResourceGroupName  "myResourceGroupScaleSet" `
+  -Location EastUS `
+  -Name myFrontendNSG `
+  -SecurityRules $nsgFrontendRule
+
+$vnet = Get-AzVirtualNetwork `
+  -ResourceGroupName  "myResourceGroupScaleSet" `
+  -Name myVnet
+
+$frontendSubnet = $vnet.Subnets[0]
+
+$frontendSubnetConfig = Set-AzVirtualNetworkSubnetConfig `
+  -VirtualNetwork $vnet `
+  -Name mySubnet `
+  -AddressPrefix $frontendSubnet.AddressPrefix `
+  -NetworkSecurityGroup $nsgFrontend
+
+Set-AzVirtualNetwork -VirtualNetwork $vnet
+
+# Update the scale set and apply the Custom Script Extension to the VM instances
+Update-AzVmss `
+  -ResourceGroupName "myResourceGroupScaleSet" `
+  -Name "myScaleSet" `
+  -VirtualMachineScaleSet $vmss
+
+# Test your application
+
+Get-AzPublicIPAddress `
+  -ResourceGroupName "myResourceGroupScaleSet" `
+  -Name "myPublicIPAddress" | select IpAddress
+
+# Configure autoscales rules
+
+# Define your scale set information
+$mySubscriptionId = (Get-AzSubscription)[0].Id
+$myResourceGroup = "myResourceGroupScaleSet"
+$myScaleSet = "myScaleSet"
+$myLocation = "East US"
+$myScaleSetId = (Get-AzVmss -ResourceGroupName $myResourceGroup -VMScaleSetName $myScaleSet).Id 
+
+# Create a scale up rule to increase the number instances after 60% average CPU usage exceeded for a 5-minute period
+$myRuleScaleUp = New-AzAutoscaleRule `
+  -MetricName "Percentage CPU" `
+  -MetricResourceId $myScaleSetId `
+  -Operator GreaterThan `
+  -MetricStatistic Average `
+  -Threshold 60 `
+  -TimeGrain 00:01:00 `
+  -TimeWindow 00:05:00 `
+  -ScaleActionCooldown 00:05:00 `
+  -ScaleActionDirection Increase `
+  -ScaleActionValue 1
+
+# Create a scale down rule to decrease the number of instances after 30% average CPU usage over a 5-minute period
+$myRuleScaleDown = New-AzAutoscaleRule `
+  -MetricName "Percentage CPU" `
+  -MetricResourceId $myScaleSetId `
+  -Operator LessThan `
+  -MetricStatistic Average `
+  -Threshold 30 `
+  -TimeGrain 00:01:00 `
+  -TimeWindow 00:05:00 `
+  -ScaleActionCooldown 00:05:00 `
+  -ScaleActionDirection Decrease `
+  -ScaleActionValue 1
+
+# Create a scale profile with your scale up and scale down rules
+$myScaleProfile = New-AzAutoscaleProfile `
+  -DefaultCapacity 2  `
+  -MaximumCapacity 10 `
+  -MinimumCapacity 2 `
+  -Rule $myRuleScaleUp,$myRuleScaleDown `
+  -Name "autoprofile"
+
+# Apply the autoscale rules
+Add-AzAutoscaleSetting `
+  -Location $myLocation `
+  -Name "autosetting" `
+  -ResourceGroup $myResourceGroup `
+  -TargetResourceId $myScaleSetId `
+  -AutoscaleProfile $myScaleProfile
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

45479935496e5551d936f199a099e1aabdd1da5a
jperez1486 <jerryperez06@gmail.com>
2020-01-09 16:56:10 -0500

    Add files via upload

---
+++ remove-files-older-than-14-days.ps1
@@ -0,0 +1,7 @@
+﻿
+$Path = "C:\backups"
+$Daysback = "-14"
+ 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item

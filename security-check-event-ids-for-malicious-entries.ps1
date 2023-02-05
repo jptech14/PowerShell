69d4ce8f39585b648799b1e32009e79916e977e2
JP <jerryperez06@gmail.com>
2023-02-05 13:59:28 -0500

    Add files via upload

---
+++ security-check-event-ids-for-malicious-entries.ps1
@@ -0,0 +1,8 @@
+﻿# Retrieve Potentially Malicious PowerShell Event Log Entries using Event ID
+$id = "4104"
+$events = Get-WinEvent -FilterHashtable @{ Path='C:\Users\Administrator\Downloads\pwsh.evtx'; Id=$id }
+$events | Select ID, Message
+
+# Query Event Log Entries to Retrieve Malicious PowerShell Commands
+$events = Get-WinEvent -Path 'C:\Users\Administrator\Downloads\pwsh.evtx' | Where-Object {$_.Message -like '*PowerShell*'}
+$events | Select ID, Message

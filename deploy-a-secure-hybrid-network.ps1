5ff98e7a8d1b596147d9812950a04acc063536da
JP <jerryperez06@gmail.com>
2023-02-05 11:23:34 -0500

    Add files via upload

---
+++ deploy-a-secure-hybrid-network.ps1
@@ -0,0 +1,5 @@
+﻿# Deploy a secure hybrid network
+# 
+
+New-AzSubscriptionDeployment -Location eastus `
+    -TemplateUri https://raw.githubusercontent.com/mspnp/samples/master/solutions/secure-hybrid-network/azuredeploy.json
---
+++ update-ssms-ps-script.ps1
@@ -0,0 +1,4 @@
+﻿$InstallerSQL = $env:TEMP + "\SSMS-Setup-ENU.exe";
+Invoke-WebRequest "https://aka.ms/ssmsfullsetup" -OutFile $InstallerSQL; 
+start $InstallerSQL /Quiet
+Remove-Item $InstallerSQL;

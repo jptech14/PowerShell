72d06020f22445e66d5005c70eaba4c70f32c48d
JP <jerryperez06@gmail.com>
2023-06-16 16:48:41 -0400

    Add files via upload
    Need Global Admin credentials.

---
+++ connect-to-azure-ad-with-powershell.ps1
@@ -0,0 +1,9 @@
+﻿## install the powershell module first then update
+## connect as the global admin, have the credentials ready.
+
+Install-Module -Name AzureAD
+
+Update-Module -Name AzureAD
+
+Connect-AzureAD
+

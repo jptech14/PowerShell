aef453f943875d31c5e4f751fce0dadbc97b2a7f
JP <jerryperez06@gmail.com>
2023-06-21 10:44:54 -0400

    Add files via upload
    Need to change two parameters and need a global admin account.
    Need to install the SharePoint PowerShell module also.

---
+++ sharepoint-check-which-spsites-a-user-has-access-to.ps1
@@ -0,0 +1,26 @@
+﻿# This script will check which SharePoint sites a user has access to.
+# Change the 'yourcompany-admin.sharepoint.com' to the real SharePoint admin site.
+# change the user email address to the user's email address
+
+# Connect to SharePoint
+Connect-SPOService -Url https://yourcompany-admin.sharepoint.com/
+
+# Set the user's email address
+$userEmail = "user@company.com"
+
+# Get all site collections in the tenant
+$sites = Get-SPOSite -Limit All
+
+# Loop through each collection and check if the user has access
+foreach ($site in $sites) {
+
+    # Get the user's permissions for the site collection
+
+    $permissions = Get-SPOUser -Site $site.Url -LoginName $userEmail -ErrorAction SilentlyContinue
+    if ($permissions) {
+
+        # If the user has permissions, output the site collection URL
+
+        Write-Host "User $($userEmail) has access to $($site.Url)"
+    }
+}
